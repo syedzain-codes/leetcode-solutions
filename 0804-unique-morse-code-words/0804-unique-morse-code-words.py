4fb4e87468febb06aebd3b3a0b4e6f81732cc24d
ZAIN <itszainahmed96800@gmail.com>
2026-07-05 17:10:32 +0530

    Time: 4 ms (21.9%), Space: 12.5 MB (18.41%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        transformations = set()
+        for word in words:
+            trans = "" 
+            for c in word:
+                trans += morse[ord(c) - ord('a')]
+            transformations.add(trans)
+        return len(transformations)
+        
