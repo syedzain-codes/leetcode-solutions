2603ac972d4d68b1dda894f5f62c2544dde1dcd3
ZAIN <itszainahmed96800@gmail.com>
2026-07-29 18:40:31 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.12%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -10,14 +10,17 @@ class Solution(object):
         ans = []
         prev = []
         
-        for i in range(1, numRows + 1):          
-            curr = [1] * i                       
-            
-            for j in range(1, i - 1):
-                curr[j] = prev[j - 1] + prev[j]
-            
-            ans.append(curr)
-            prev = curr                         
+        
+        for i in range(1, numRows + 1): 
+            temp = []                     
+            for j in range(1, i + 1):     
+                if j == 1 or j == i:      
+                    temp.append(1)
+                else:                     
+                    
+                    temp.append(prev[j-2] + prev[j-1])
+            ans.append(temp)
+            prev = temp                  
         
         return ans
 
