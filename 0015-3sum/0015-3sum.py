7424f2ef67170c489bd3caed4c63f016d9e55f2c
ZAIN <itszainahmed96800@gmail.com>
2026-09-26 15:44:37 +0530

    Time: 856 ms (22.81%), Space: 18.2 MB (81.84%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,33 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        res=[]
         nums.sort()
-        for i in range(0,len(nums)):
-            if i>0 and nums[i-1]==nums[i]:
+        res=[]
+        for i in range(0,len(nums)-2):
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            l,r=i+1,len(nums)-1
+
+            l=i+1
+            r=len(nums)-1
             while(l<r):
-                threesum=nums[i]+nums[l]+nums[r]
-                if threesum>0:
-                    r-=1
-                elif threesum<0:
+                s=nums[i]+nums[r]+nums[l]
+                if s==0:
+                    res.append([nums[i],nums[l],nums[r]])
+                    while l<r and nums[l]==nums[l+1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r-1]:
+                        r-=1
                     l+=1
-                else:
-                
-                    res.append([nums[i], nums[l], nums[r]])
-
-                    l += 1
-                    r -= 1
-
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
+                    r-=1
+                if s>0:
+                    r-=1
+                if s<0:
+                    l+=1
         return res
 
-
-                
-            
         
